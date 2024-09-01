5effaa8e670b608715ae4ce58a253ced839fe5dc
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-09-01 17:59:14 +0530

    dashboard update

--- resources/views/layout/authLay.blade.php
+++ resources/views/layout/authLay.blade.php
@@ -42,10 +42,14 @@
     <!-- Include Slick JS (you can use a CDN) -->
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
+    <!-- Bootstrap icons -->\
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
-    <div class="container">
+    <div class="container-fluid main_container">
         <!-- {{-- successfull message --}} -->
         @if (Session::has('success'))
             <div class="toast-container position-fixed top-0 end-0 p-3">
@@ -87,7 +91,9 @@
         @endif
 
         @yield('content')
+
     </div>
+    
 </body>
 
 </html>
